c599bdb9882ad9c91ba0a460ff1e1bad84fd7b7e
xiaoxiaocat <ycc1088@gmial.com>
2022-09-06 01:06:40 +1200

    got tabs issue

--- JS/app.js
+++ JS/app.js
@@ -13,11 +13,11 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 1,
-    jobType: "C",
+    jobType: "E",
     jobNo: 1,
     customer: "Flip Farm",
     jobName: "Infeed Top Frame",
@@ -29,7 +29,7 @@ var list = [
     mandrelIsReady: true,
     mandrelIsOrdered: false,
     isFinish: true,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 2,
@@ -45,7 +45,7 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: true,
     isFinish: true,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 3,
@@ -61,7 +61,7 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 4,
@@ -77,7 +77,7 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 5,
@@ -93,7 +93,7 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 6,
@@ -109,7 +109,7 @@ var list = [
     mandrelIsReady: true,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 7,
@@ -125,7 +125,7 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 8,
@@ -141,7 +141,7 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
   },
   {
     id: 9,
@@ -157,13 +157,172 @@ var list = [
     mandrelIsReady: false,
     mandrelIsOrdered: false,
     isFinish: false,
-    isEdit: false
+    isEdit: false,
+  },
+  {
+    id: 10,
+    jobType: "B",
+    jobNo: 0,
+    customer: "Flip Farm",
+    jobName: "Infeed Top Frame",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 11,
+    jobType: "E",
+    jobNo: 1,
+    customer: "Flip Farm",
+    jobName: "Infeed Top Frame",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: true,
+    mandrelIsOrdered: false,
+    isFinish: true,
+    isEdit: false,
+  },
+  {
+    id: 12,
+    jobType: "E",
+    jobNo: 2,
+    customer: "ZWH",
+    jobName: "S4076N76",
+    qty: 60,
+    process: "76076S",
+    material: "L407616D",
+    materialIsReady: true,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: true,
+    isFinish: true,
+    isEdit: false,
+  },
+  {
+    id: 13,
+    jobType: "B",
+    jobNo: 3,
+    customer: "IMG001",
+    jobName: "Test Frame",
+    qty: 40,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 14,
+    jobType: "B",
+    jobNo: 4,
+    customer: "CHEN001",
+    jobName: "Outfeed Top Frame",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 15,
+    jobType: "E",
+    jobNo: 5,
+    customer: "STAY002",
+    jobName: "PUSH BEND",
+    qty: 10,
+    process: "44044S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: true,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 16,
+    jobType: "A",
+    jobNo: 6,
+    customer: "EHMO002",
+    jobName: "Rolling Cages",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: true,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 17,
+    jobType: "A",
+    jobNo: 7,
+    customer: "Test 7",
+    jobName: "Infeed Top Frame",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 18,
+    jobType: "B",
+    jobNo: 8,
+    customer: "Test 8",
+    jobName: "Infeed Top Frame",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: false,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
+  },
+  {
+    id: 19,
+    jobType: "E",
+    jobNo: 0,
+    customer: "HAHA10",
+    jobName: "Rolling",
+    qty: 10,
+    process: "33066S",
+    material: "L6P025NB27",
+    materialIsReady: true,
+    materialIsOrdered: false,
+    mandrelIsReady: false,
+    mandrelIsOrdered: false,
+    isFinish: false,
+    isEdit: false,
   },
 ];
 
-
 // var list = JSON.parse(window.localStorage.getItem('todos'))||[];
-var type="all";
+var type = "all";
 //box = container
 var box = document.querySelector(".todoapp");
 // box.innerHTML = template("tmp", {});
@@ -171,6 +330,10 @@ bindHtml();
 // 2 Render function
 function bindHtml() {
   var bindlist = list;
+
+  let csm100IsActive=true;
+  let csm38IsActive=false;
+  let engIsActive=false;
 
   switch (type) {
     case "all":
@@ -188,15 +351,29 @@ function bindHtml() {
   }
 
   // joblists for csm100 csm38 and engineering
-  var aList=bindlist.filter(function (t) {return t.jobType == "A"});
-  aList.sort((a,b) => a.jobNo-b.jobNo);
-  aList.forEach(function (t,index) { return t.jobNo=index+1}); 
+  var aList = bindlist.filter(function (t) {
+    return t.jobType == "A";
+  });
+  aList.sort((a, b) => a.jobNo - b.jobNo);
+  aList.forEach(function (t, index) {
+    return (t.jobNo = index + 1);
+  });
 
-  var bList=bindlist.filter(function (t) {return t.jobType == "B"});
+  var bList = bindlist.filter(function (t) {
+    return t.jobType == "B";
+  });
+  bList.sort((a, b) => a.jobNo - b.jobNo);
+  bList.forEach(function (t, index) {
+    return (t.jobNo = index + 1);
+  });
 
-  var eList=bindlist.filter(function (t) {return t.jobType == "E"});
-
-
+  var eList = bindlist.filter(function (t) {
+    return t.jobType == "E";
+  });
+  eList.sort((a, b) => a.jobNo - b.jobNo);
+  eList.forEach(function (t, index) {
+    return (t.jobNo = index + 1);
+  });
 
   //计算所有未完成的数量
   var activeNum = list.filter(function (t) {
@@ -204,22 +381,58 @@ function bindHtml() {
   }).length;
   //number of csm100 job
   var activeANum = list.filter(function (t) {
-    return (!t.isFinish & t.jobType=="A");
+    return !t.isFinish & (t.jobType == "A");
   }).length;
   var activeBNum = list.filter(function (t) {
-    return (!t.isFinish & t.jobType=="B");
+    return !t.isFinish & (t.jobType == "B");
   }).length;
   var activeENum = list.filter(function (t) {
-    return (!t.isFinish & t.jobType=="E");
+    return !t.isFinish & (t.jobType == "E");
   }).length;
 
   let aLength = aList.length;
   let bLength = bList.length;
   let eLength = eList.length;
-  
+
+  // let tabs = document.querySelectorAll(".nav-link");
+
+  // let activeTab = document.querySelector(".nav-link,.active");
+  // tabs.forEach((t) => t.setAttribute("className", ".nav-link"));
+  // console.log(tabs);
+  // activeTab.setAttribute("className", ".nav-link,.active");
+
+  // let panes = document.querySelectorAll(".nav-pane");
+  // let activePane= document.querySelector(".nav-pane, .active");
+  // panes=Array.from(panes);
+  // activePane=[].slice.call(activePane);
+  // let index= panes.findIndex((t)=> t==activePane);
+  // console.log(index);
+  // console.log(activePane.attributes.className);
+  // if(index==0){
+  //   csm100IsActive=true;
+  //   csm38IsActive=false;
+  //   engIsActive=false;
+  // }
+  // if(index==1){
+  //   csm100IsActive=false;
+  //   csm38IsActive=true;
+  //   engIsActive=false;
+  // }
+
+  // if(index==2){
+  //   csm100IsActive=false;
+  //   csm38IsActive=false;
+  //   engIsActive=true;
+  // }
+  let activePane= document.querySelector(".nav-pane, .active");
+  console.log(activePane)
+ 
 
   box.innerHTML = template("tmp", {
     bindlist: bindlist,
+    // csm100IsActive,
+    // csm38IsActive,
+    // engIsActive,
     activeNum: activeNum,
     activeANum,
     activeBNum,
@@ -233,8 +446,12 @@ function bindHtml() {
     length: list.length,
     type: type,
   });
-  console.log(activeANum, aLength);
-  window.localStorage.setItem('todos',JSON.stringify(list));
+  let panes = document.querySelectorAll(".nav-pane");
+  panes.forEach(t=>t.classlist.remove("active"));
+  activePane.classList.add("active");
+  console.log(activePane);
+
+  window.localStorage.setItem("todos", JSON.stringify(list));
 }
 //根据地址栏哈希值改变显示
 window.addEventListener("hashchange", function (e) {
@@ -242,7 +459,7 @@ window.addEventListener("hashchange", function (e) {
   bindHtml();
 });
 
-//4 事件委托的形式添加  因为input会变化 事件绑在了container 上
+//4 事件委托的形式添加
 box.addEventListener("keydown", function (e) {
   e = e || window.event;
   var target = e.target || e.srcElement;
@@ -254,10 +471,9 @@ box.addEventListener("keydown", function (e) {
     var text = target.value.trim();
     if (!text) return;
     var id = 0;
-   
+
     if (list.length) {
       id = list[list.length - 1].id + 1;
-     
     } else {
       id = 1;
     }
@@ -266,29 +482,31 @@ box.addEventListener("keydown", function (e) {
   }
   //10 在编辑的文本里回车  确认编辑  如果文本为空 删除  不为空 修改
   if (target.className === "edit" && code === 13) {
-    var text= target.value.trim();
-    var id = target.dataset.id -0;
-    if(!text){
-      list=list.filter(function(item){return item.id !== id;});
-    }
-    else{
-      var todo = list. find(function(t){return t.id===id;});
-      todo.content =text;
+    var text = target.value.trim();
+    var id = target.dataset.id - 0;
+    if (!text) {
+      list = list.filter(function (item) {
+        return item.id !== id;
+      });
+    } else {
+      var todo = list.find(function (t) {
+        return t.id === id;
+      });
+      todo.content = text;
       todo.isEdit = false;
     }
     bindHtml();
   }
 
   //取消编辑
-  if (target.className === "edit" && code === 27){
-  
-    var id = target.dataset.id -0;
-    var todo = list.find(function(t){return t.id===id;});
-    todo.isEdit=false;
+  if (target.className === "edit" && code === 27) {
+    var id = target.dataset.id - 0;
+    var todo = list.find(function (t) {
+      return t.id === id;
+    });
+    todo.isEdit = false;
     bindHtml();
-
   }
-    
 });
 
 //5 事件的委托形式出现点击
@@ -341,9 +559,14 @@ box.addEventListener("click", function (e) {
     let todo = list.find(function (t) {
       return t.id === id;
     });
-    
-    list.forEach(function (t) {if(t.jobType === todo.jobType && t.jobNo===todo.jobNo-1){ t.jobNo=t.jobNo+1;return}})
-      todo.jobNo = todo.jobNo-1;
+
+    list.forEach(function (t) {
+      if (t.jobType === todo.jobType && t.jobNo === todo.jobNo - 1) {
+        t.jobNo = t.jobNo + 1;
+        return;
+      }
+    });
+    todo.jobNo = todo.jobNo - 1;
     bindHtml();
   }
   if (target.dataset.key === "down") {
@@ -351,96 +574,113 @@ box.addEventListener("click", function (e) {
     let todo = list.find(function (t) {
       return t.id === id;
     });
-    
-    list.forEach(function (t) {if(t.jobType === todo.jobType && t.jobNo===todo.jobNo+1){ t.jobNo=t.jobNo-1;return}})
-      todo.jobNo = todo.jobNo+1;
+
+    list.forEach(function (t) {
+      if (t.jobType === todo.jobType && t.jobNo === todo.jobNo + 1) {
+        t.jobNo = t.jobNo - 1;
+        return;
+      }
+    });
+    todo.jobNo = todo.jobNo + 1;
     bindHtml();
   }
   if (target.dataset.key === "del") {
     let id = target.dataset.id - 0;
-    let index = list.findIndex(function (t){return t.id === id;})
-    list.splice(index,1);
-        bindHtml();
+    let index = list.findIndex(function (t) {
+      return t.id === id;
+    });
+    list.splice(index, 1);
+    bindHtml();
   }
   if (target.dataset.key === "clear-completed") {
-   
-   list=list.filter(t=>(t.jobType==target.dataset.type && !t.isFinish))
-        bindHtml();
+    list = list.filter((t) => t.jobType == target.dataset.type && !t.isFinish);
+    bindHtml();
   }
   if (target.dataset.key === "add") {
-   
-    let customer= getElementById("customer").value.trim();
-    .value.trim();
-    jobType: "B",
-    jobNo: 0,
-    customer: "Flip Farm",
-    jobName: "Infeed Top Frame",
-    qty: 10,
-    process: "33066S",
-    material: "L6P025NB27",
-    materialIsReady: false,
-    materialIsOrdered: false,
-    mandrelIsReady: false,
-    mandrelIsOrdered: false,
-    isFinish: false,
-    isEdit: false
-    if (!text) return;
+    let customer = document.getElementById("customer").value.trim();
+    let jobName = document.getElementById("job").value.trim();
+    let qty = document.getElementById("qty").value.trim();
+    let process = document.getElementById("process").value.trim();
+    let material = document.getElementById("material").value.trim();
+
+    if (!customer || !jobName || !qty || !process || !material) return;
     var id = 0;
-   
+
     if (list.length) {
       id = list[list.length - 1].id + 1;
-     
     } else {
       id = 1;
     }
-    list.push({ id: id, content: text, isFinish: false, isEdit: false });
+    list.push({
+      id: id,
+      jobType: target.dataset.type,
+      customer,
+      jobName,
+      qty,
+      process,
+      material,
+      jobType: target.dataset.type,
+      materialIsReady: false,
+      materialIsOrdered: false,
+      mandrelIsReady: false,
+      mandrelIsOrdered: false,
+      isFinish: false,
+      isEdit: false,
+    });
     bindHtml();
-        
-   }
+  }
 });
 
-
-//9 进入编辑状态 双击事件委托 
+//9 进入编辑状态 双击事件委托
 
 box.addEventListener("dblclick", function (e) {
   e = e || window.event;
   var target = e.target || e.srcElement;
-  let originalValue=target.innerHTML;
-  let inputCount=document.getElementsByClassName('active-input');
-  if(inputCount.length==0 && (target.dataset.key==="customer" || target.dataset.key==="jobName" || target.dataset.key==="qty" || target.dataset.key==="process") ){
-    let input=document.createElement("input");
-    target.innerHTML='';
+  let originalValue = target.innerHTML;
+  let inputCount = document.getElementsByClassName("active-input");
+  if (
+    inputCount.length == 0 &&
+    (target.dataset.key === "customer" ||
+      target.dataset.key === "jobName" ||
+      target.dataset.key === "qty" ||
+      target.dataset.key === "process")
+  ) {
+    let input = document.createElement("input");
+    target.innerHTML = "";
     target.appendChild(input);
     input.setAttribute("class", "active-input");
-    let width=target.offsetWidth;
+    let width = target.offsetWidth;
     console.log(width);
     //此处宽度跟着变化  不好使 需要修正
-    input.setAttribute("style", "width:"+width+"px");
-    input.value=originalValue;
+    input.setAttribute("style", "width:" + width + "px");
+    input.value = originalValue;
     input.focus();
-    input.onblur=function(){
-      target.innerHTML=input.value;
-      let id = target.dataset.id-0;
-      let todo=list.find(function(t){return t.id===id});
-      todo[target.dataset.key]=input.value;
+    input.onblur = function () {
+      target.innerHTML = input.value;
+      let id = target.dataset.id - 0;
+      let todo = list.find(function (t) {
+        return t.id === id;
+      });
+      todo[target.dataset.key] = input.value;
       bindHtml();
-    }
+    };
   }
-  if(inputCount.length==0 && (target.dataset.key==="material" )){
-    let input=document.createElement("input");
-    target.innerHTML='';
+  if (inputCount.length == 0 && target.dataset.key === "material") {
+    let input = document.createElement("input");
+    target.innerHTML = "";
     target.appendChild(input);
     input.setAttribute("class", "active-input");
-    let index=originalValue.indexOf("<button")
-    input.value=originalValue.slice(0,index).trim();
+    let index = originalValue.indexOf("<button");
+    input.value = originalValue.slice(0, index).trim();
     input.focus();
-    input.onblur=function(){
-      target.innerHTML=input.value;
-      let id = target.dataset.id-0;
-      let todo=list.find(function(t){return t.id===id});
-      todo[target.dataset.key]=input.value;
+    input.onblur = function () {
+      target.innerHTML = input.value;
+      let id = target.dataset.id - 0;
+      let todo = list.find(function (t) {
+        return t.id === id;
+      });
+      todo[target.dataset.key] = input.value;
       bindHtml();
-    }
+    };
   }
-
 });
